db8877b7f27706b93eeca9abb187cc54990da551
liamwb <liam.woodbaker@gmail.com>
2018-08-28 19:11:21 +1000

    Fixed the findFactors method in Methods class.
    It wasn't working because the boolean clause was written incorrectly.

--- src/myPackage/Methods.java
+++ src/myPackage/Methods.java
@@ -28,16 +28,15 @@ public class Methods {
 	
 	public static BigInteger[] findFactors (BigInteger number) {
 		List<BigInteger> factors = new ArrayList<BigInteger>(); //A list so that factors can be easily appended as they are confirmed
-		
-		for (BigInteger i = BigInteger.valueOf(2); i.compareTo(number.add(BigInteger.ONE)) == -1; i.add(BigInteger.ONE)) {
-			System.out.println("i is " +i);
-		//this is the same as for (i = 1; i + 1 < number; i++), but using BigInteger because these numbers may get very large
-			if (i.mod(number).equals(BigInteger.ZERO)) {
-			//if i % number == 0
-				factors.add(i);
-				System.out.println(1 + "is a factor");
+
+		for (BigInteger i = BigInteger.valueOf(2); i.subtract(BigInteger.ONE).compareTo(BigInteger.valueOf(6)) == -1; i = i.add(BigInteger.ONE)) {
+			//this is the same as for (i = 2; i + 1 < number; i++), but using BigInteger because these numbers may get very large
+				
+				if (number.remainder(i).equals(BigInteger.valueOf(0))) {
+					factors.add(i);
+					i.add(BigInteger.ONE);
+				}
 			}
-		}
 		//putting the factors into an array. Arrays are faster than lists.
 		BigInteger[] array = new BigInteger[factors.size()];
 		for (int i = 0; i < factors.size(); i++) {
@@ -47,7 +46,7 @@ public class Methods {
 	}
 	
 	public static BigInteger findE(BigInteger L, BigInteger N) {
-		boolean found = false; BigInteger T = BigInteger.ONE; //since the while loop starts with T++, we will not test 1 as a factor
+		boolean found = false; BigInteger T = BigInteger.valueOf(2); //since the while loop starts with T++, we will not test 1 as a factor
 		
 /*The rule for E is that E is coprime with L & N (i.e. E, L, and N share no common factors (except 1)).
  * to find a suitable E, this method will, starting at 1, take some test number, T, and find its factors. Then,
@@ -58,12 +57,30 @@ public class Methods {
  * Additionally, 1 < E < L
  */
 		while (!found) {
-			if (T.equals(L)) return BigInteger.ONE; //T is less than L
+			if (T.equals(L)) return BigInteger.valueOf(-1); //T is less than L
 			
+			BigInteger[] Tfactors = findFactors(T); boolean hasCommonFactor = false;
+			
+			for (int i = 0; i < Tfactors.length; i++) {
+				if (Tfactors[i].mod(L).equals(BigInteger.ZERO)) {
+					hasCommonFactor = true;
+					i = Tfactors.length;
+					System.out.println(i + " is a factor of " + T + ", and goes into" + L);
+				}
+				if (Tfactors[i].mod(N).equals(BigInteger.ZERO)) {
+					hasCommonFactor = true;
+					i = Tfactors.length;
+					System.out.println(i + " is a factor of " + N + ", and goes into" + L);
+				}
+				//ie if any factor of T goes into L or N, then T is not a suitable candidate for E,
+				//and so the for loop is exited and T is incremented by 1
+			}
+			if (!hasCommonFactor) {
+				found = true;
+				break;
+			}
 			T.add(BigInteger.ONE); //T++
-			BigInteger[] Tfactors = findFactors(T); boolean testFailed = false;
-			int i = 0;
-			while (!testFailed) {
+			/*while (!testFailed) {
 				if (Tfactors[i].mod(L).equals(BigInteger.ZERO)) {
 					i++; testFailed = true;
 				}
@@ -71,8 +88,7 @@ public class Methods {
 					i++; testFailed = true;
 				}
 			found = true;
-			}
-			
+			}*/	
 		}
 		return T;
 	}
--- src/myPackage/testing.java
+++ src/myPackage/testing.java
@@ -2,11 +2,47 @@ package myPackage;
 
 import java.applet.*;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class testing extends Applet {
+	
+	public static int[] findFactors (int number) {
+		
+		List<Integer> factors = new ArrayList<Integer>(); //A list so that factors can be easily appended as they are confirmed
+		
+		for (int i = 2; i + 1 < number; i++) {
+		//this is the same as for (i = 1; i + 1 < number; i++), but using BigInteger because these numbers may get very large
+			if (number % i == 0) {
+			//if i % number == 0
+				factors.add(i);
+			}
+		}
+		//putting the factors into an array. Arrays are faster than lists.
+		int[] array = new int[factors.size()];
+		for (int i = 0; i < factors.size(); i++) {
+			array[i] = factors.get(i);
+		}
+		return array;
+	}
+	
+	
 	public void init() {
-		System.out.println(Methods.findN(BigInteger.valueOf(2), BigInteger.valueOf(7)));
-		System.out.println(Methods.findL(BigInteger.valueOf(2), BigInteger.valueOf(7)));
-		System.out.println(Methods.findE(BigInteger.valueOf(2), BigInteger.valueOf(7)));
+		System.out.print(BigInteger.valueOf(6).remainder(BigInteger.valueOf(3)));
+		//System.out.println(Methods.findFactors(BigInteger.valueOf(6)));
+		
+		for (int i = 0; i < Methods.findFactors(BigInteger.valueOf(6)).length; i++) {
+			System.out.println(Methods.findFactors(BigInteger.valueOf(6))[i]);
+		}
+		
+		/*for (BigInteger i = BigInteger.valueOf(2); i.add(BigInteger.ONE).compareTo(BigInteger.valueOf(6)) == -1; i.add(BigInteger.ONE)) {
+			System.out.println(i);
+			System.out.println(i.add(BigInteger.ONE).compareTo(BigInteger.valueOf(6)) == -1);
+			i.add(BigInteger.ONE);
+		}*/
+		
+		
+		//for (BigInteger i = BigInteger.valueOf(2); i.subtract(BigInteger.ONE).compareTo(BigInteger.valueOf(6)) == -1; i = i.add(BigInteger.ONE)) {
+
 	}
 }
